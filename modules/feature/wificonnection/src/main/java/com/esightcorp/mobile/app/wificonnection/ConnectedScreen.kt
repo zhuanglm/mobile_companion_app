161da1d7a62de668bd2c2086af8697f2f20b9df1
Phan Dang Duy Thinh <phduxa@syntronic.com>
2023-12-04 18:28:05 -0500

    WifiSetup - Clean up backstack after a successful wifi configuration

--- modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/ConnectedScreen.kt
+++ modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/ConnectedScreen.kt
@@ -1,40 +1,59 @@
 package com.esightcorp.mobile.app.wificonnection
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.esightcorp.mobile.app.ui.components.loading.LoadingScreenWithIcon
-import com.esightcorp.mobile.app.wificonnection.viewmodels.ConnectedViewModel
-import kotlinx.coroutines.delay
+import androidx.navigation.compose.rememberNavController
 import com.esightcorp.mobile.app.ui.R
+import com.esightcorp.mobile.app.ui.components.loading.LoadingScreenWithIcon
+import com.esightcorp.mobile.app.ui.components.viewmodel.ESightBaseViewModel
+import com.esightcorp.mobile.app.ui.navigation.OnNavigationCallback
+import com.esightcorp.mobile.app.ui.navigation.WifiNavigation
+import kotlinx.coroutines.delay
 
 @Composable
 fun ConnectedRoute(
     navController: NavController,
-    vm: ConnectedViewModel = hiltViewModel()
-){
+    vm: ESightBaseViewModel = hiltViewModel()
+) {
     ConnectedScreen(
         navController = navController,
         modifier = Modifier,
-
+        onGotoHomeScreen = { nav ->
+            vm.gotoMainScreen(nav, popUntil = WifiNavigation.IncomingRoute)
+        },
     )
 }
+
+//region Internal implementation
 
 @Composable
-internal fun ConnectedScreen(
+private fun ConnectedScreen(
     modifier: Modifier = Modifier,
-    navController: NavController
-){
-    val homeRoute = "home_first"
-    val screenTimeout = 5000L //5s in milliseconds
+    navController: NavController,
+    onGotoHomeScreen: OnNavigationCallback? = null,
+) {
     LoadingScreenWithIcon(
-        loadingText = stringResource(id = R.string.wifi_connected_text),
+        modifier = modifier,
+        loadingText = stringResource(R.string.wifi_connected_text),
     )
+
     LaunchedEffect(Unit) {
-        delay(screenTimeout)
-        navController.navigate(homeRoute)
+        delay(SCREEN_TIMEOUT)
+        onGotoHomeScreen?.invoke(navController)
     }
 }
+
+@Preview
+@Composable
+private fun ConnectedScreenPreview() = MaterialTheme {
+    ConnectedScreen(navController = rememberNavController())
+}
+
+private const val SCREEN_TIMEOUT = 5000L //5s in milliseconds
+//endregion

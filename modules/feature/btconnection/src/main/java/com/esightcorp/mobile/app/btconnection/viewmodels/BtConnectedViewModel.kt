0774de48c3cfe5ef58c9251083c1ea19e927ccb5
Phan Dang Duy Thinh <phduxa@syntronic.com>
2023-12-05 09:37:55 -0500

    BtConnection - Clean up backstack after a successful connect

--- modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/viewmodels/BtConnectedViewModel.kt
+++ modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/viewmodels/BtConnectedViewModel.kt
@@ -3,10 +3,13 @@ package com.esightcorp.mobile.app.btconnection.viewmodels
 import android.annotation.SuppressLint
 import android.app.Application
 import android.bluetooth.BluetoothDevice
-import androidx.lifecycle.AndroidViewModel
-import com.esightcorp.mobile.app.btconnection.repositories.BtConnectionRepository
+import androidx.navigation.NavController
 import com.esightcorp.mobile.app.btconnection.repositories.BluetoothConnectionRepositoryCallback
+import com.esightcorp.mobile.app.btconnection.repositories.BtConnectionRepository
 import com.esightcorp.mobile.app.btconnection.state.BtConnectedUiState
+import com.esightcorp.mobile.app.ui.components.viewmodel.ESightBaseViewModel
+import com.esightcorp.mobile.app.ui.extensions.navigate
+import com.esightcorp.mobile.app.ui.navigation.BtConnectionNavigation
 import com.esightcorp.mobile.app.utils.ScanningStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,17 +18,17 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
-
 @HiltViewModel
 class BtConnectedViewModel @Inject constructor(
-    application: Application, private val repository: BtConnectionRepository
-) : AndroidViewModel(application) {
+    application: Application,
+    private val repository: BtConnectionRepository,
+) : ESightBaseViewModel(application) {
 
-    private val TAG = "BtConnectedViewModel"
     private var _uiState = MutableStateFlow(BtConnectedUiState())
     val uiState: StateFlow<BtConnectedUiState> = _uiState.asStateFlow()
-    val listener = object : BluetoothConnectionRepositoryCallback{
-        override fun scanStatus(isScanning: ScanningStatus){
+
+    private val listener = object : BluetoothConnectionRepositoryCallback {
+        override fun scanStatus(isScanning: ScanningStatus) {
             //unused by this composable
         }
 
@@ -47,6 +50,10 @@ class BtConnectedViewModel @Inject constructor(
         getBluetoothDevice()
     }
 
+    fun gotoNoDeviceConnectedScreen(navController: NavController) = with(navController) {
+        navigate(BtConnectionNavigation.NoDeviceConnectedRoute)
+    }
+
     @SuppressLint("MissingPermission")
     private fun getBluetoothDevice() {
         val device = repository.getConnectedDevice()
@@ -60,11 +67,9 @@ class BtConnectedViewModel @Inject constructor(
         }
     }
 
-    private fun updateIsBtEnabledState(enabled: Boolean){
+    private fun updateIsBtEnabledState(enabled: Boolean) {
         _uiState.update { state ->
             state.copy(isBtEnabled = enabled)
         }
     }
-
-
 }

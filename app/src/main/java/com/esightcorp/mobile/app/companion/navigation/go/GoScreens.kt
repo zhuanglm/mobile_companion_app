1b10e6a5e280bc7be2420ab2bcd66866830caf7b
Phan Dang Duy Thinh <phduxa@syntronic.com>
2023-12-06 17:49:49 -0500

    Clean up code in application

--- app/src/main/java/com/esightcorp/mobile/app/companion/navigation/go/GoScreens.kt
+++
@@ -1,8 +0,0 @@
-//package com.esightcorp.mobile.app.companion.navigation.go
-//
-//import com.esightcorp.mobile.app.home.navigation.HomeScreens
-//
-//sealed class GoScreens(val route: String){
-//    object HomeScreen: GoScreens(HomeScreens.IncomingNavigationRoute.route)
-//
-//}

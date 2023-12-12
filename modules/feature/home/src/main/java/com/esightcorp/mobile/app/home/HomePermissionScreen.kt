595db20ba3c2179477b0231684ed657e9f14a11d
Phan Dang Duy Thinh <phduxa@syntronic.com>
2023-12-12 11:49:17 -0500

    Refine permission implementation

--- modules/feature/home/src/main/java/com/esightcorp/mobile/app/home/HomePermissionScreen.kt
+++ modules/feature/home/src/main/java/com/esightcorp/mobile/app/home/HomePermissionScreen.kt
@@ -1,78 +1,78 @@
 package com.esightcorp.mobile.app.home
 
+import android.util.Log
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.esightcorp.mobile.app.home.viewmodels.PermissionViewModel
+import com.esightcorp.mobile.app.ui.R
 import com.esightcorp.mobile.app.ui.components.containers.BaseSurface
-import com.esightcorp.mobile.app.ui.extensions.navigate
-import com.esightcorp.mobile.app.ui.navigation.BtConnectionNavigation
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 
-@Composable
-fun HomePermissionScreen(navController: NavController) {
-    BasePermissionScreen(navController = navController)
-}
-
-
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun BasePermissionScreen(
-    navController: NavController, vm: PermissionViewModel = hiltViewModel()
+fun HomePermissionScreen(
+    navController: NavController,
+    vm: PermissionViewModel = hiltViewModel(),
 ) {
-    val permissionsState = rememberMultiplePermissionsState(permissions = vm.getPermissionList())
-    if (permissionsState.allPermissionsGranted) {
-        LaunchedEffect(Unit) {
-            navController.navigate(BtConnectionNavigation.IncomingRoute)
-        }
+    val permissionsState = rememberMultiplePermissionsState(permissions = vm.requiredPermissions)
+    Log.w(
+        TAG,
+        "permissionsState:\nallPermissionsGranted: ${permissionsState.allPermissionsGranted}" +
+                "\nshouldShowRationale: ${permissionsState.shouldShowRationale}"
+    )
+    when (permissionsState.allPermissionsGranted) {
+        true -> LaunchedEffect(Unit) { vm.navigateToBluetoothConnection(navController) }
 
-    } else {
-        BaseSurface() {
+        false -> BaseSurface {
             Rationale(permissionsState = permissionsState)
         }
     }
-
 }
+
+//region Internal implementation
+
+private const val TAG = "HomePermissionScreen"
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Rationale(permissionsState: MultiplePermissionsState) {
+private fun Rationale(permissionsState: MultiplePermissionsState) {
     PermissionDialog(permissionsState = permissionsState)
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun PermissionDialog(
+private fun PermissionDialog(
     permissionsState: MultiplePermissionsState?
 ) {
-    AlertDialog(onDismissRequest = { Unit },
+    AlertDialog(
+        onDismissRequest = { },
         confirmButton = {
             Button(onClick = { permissionsState?.launchMultiplePermissionRequest() }) {
-                Text(text = "Request Permissions")
+                Text(text = stringResource(R.string.label_home_screen_request_permission_button_confirm))
             }
         },
         title = {
-            Text(text = "Permissions are required to use this app")
+            Text(text = stringResource(R.string.label_home_screen_request_permission_title))
         },
-        text = { Text(text = "These permissions allow you to have access to the full functionality of the app.") },
+        text = { Text(stringResource(R.string.label_home_screen_request_permission_description)) },
         properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
-        dismissButton = {})
-
+    )
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Preview
 @Composable
-fun PermissionDialogPreview() {
-    PermissionDialog(null)
+private fun PermissionDialogPreview() = PermissionDialog(null)
 
-}
+//endregion
--- modules/feature/home/src/main/java/com/esightcorp/mobile/app/home/viewmodels/PermissionViewModel.kt
+++ modules/feature/home/src/main/java/com/esightcorp/mobile/app/home/viewmodels/PermissionViewModel.kt
@@ -2,39 +2,33 @@ package com.esightcorp.mobile.app.home.viewmodels
 
 import android.app.Application
 import android.os.Build
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.navigation.NavController
+import com.esightcorp.mobile.app.ui.extensions.navigate
+import com.esightcorp.mobile.app.ui.navigation.BtConnectionNavigation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PermissionViewModel @Inject constructor(
     application: Application
-): AndroidViewModel(application){
+) : AndroidViewModel(application) {
 
-    /**
-     * Permissions management
-     */
+    val requiredPermissions: List<String>
+        get() = arrayListOf<String>().apply {
+            add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            add(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
-    fun getPermissionList(): List<String>{
-        val PERMISSIONS:List<String> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            listOf(
-                android.Manifest.permission.BLUETOOTH_SCAN,
-                android.Manifest.permission.BLUETOOTH_CONNECT,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        } else {
-            listOf(
-                android.Manifest.permission.BLUETOOTH,
-                android.Manifest.permission.BLUETOOTH_ADMIN,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                add(android.Manifest.permission.BLUETOOTH_SCAN)
+                add(android.Manifest.permission.BLUETOOTH_CONNECT)
+            } else {
+                add(android.Manifest.permission.BLUETOOTH)
+                add(android.Manifest.permission.BLUETOOTH_ADMIN)
+            }
         }
-        Log.d("TAG", "getBluetoothPermissionsList: ${PERMISSIONS.first()} ")
-        return PERMISSIONS
+
+    fun navigateToBluetoothConnection(navController: NavController) = with(navController) {
+        navigate(BtConnectionNavigation.IncomingRoute)
     }
-
-
 }

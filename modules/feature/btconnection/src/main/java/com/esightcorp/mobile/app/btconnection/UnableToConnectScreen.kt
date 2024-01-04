399cc288a95dea1a30a02e78e3faae20716ceb93
raymond zhuang <zhuanglm@gmail.com>
2024-01-04 13:03:57 -0500

    EG-2318: changed import from rebase from master

--- modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/UnableToConnectScreen.kt
+++ modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/UnableToConnectScreen.kt
@@ -15,10 +15,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.esightcorp.mobile.app.btconnection.viewmodels.UnableToConnectViewModel
 import com.esightcorp.mobile.app.ui.R
-import com.esightcorp.mobile.app.ui.components.BodyText
-import com.esightcorp.mobile.app.ui.components.Header1Text
-import com.esightcorp.mobile.app.ui.components.Subheader
-import com.esightcorp.mobile.app.ui.components.TextRectangularButton
+import com.esightcorp.mobile.app.ui.components.text.BodyText
+import com.esightcorp.mobile.app.ui.components.text.Header1Text
+import com.esightcorp.mobile.app.ui.components.text.Subheader
+import com.esightcorp.mobile.app.ui.components.buttons.TextRectangularButton
 import com.esightcorp.mobile.app.ui.components.buttons.bottomButtons.HowToConnectButton
 import com.esightcorp.mobile.app.ui.components.containers.BaseScreen
 import com.esightcorp.mobile.app.ui.components.help.NumberedHelpItem

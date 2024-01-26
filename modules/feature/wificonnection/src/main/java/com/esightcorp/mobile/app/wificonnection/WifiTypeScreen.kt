327752fa606459e210c3f54d335ed7bba7c600a1
raymond zhuang <zhuanglm@gmail.com>
2024-01-26 12:25:50 -0500

    EG-2469: enabled create wifi code in AdvancedWifiScreen; allow to storage a wifi network with ssid/security type; new AdvancedWifiScreen, WifiTypeScreen; fixed ssid and password fields behaviour; changed wifi security type to match resources; new icon button;

--- modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/WifiTypeScreen.kt
+++ modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/WifiTypeScreen.kt
@@ -1,33 +1,44 @@
 /*
- * LICENSE
- * Copyright (C) 2009-2024 by eSight by Gentex Corporation. All Rights Reserved.
- * The software and information contained herein are proprietary to, and
- * comprise valuable trade secrets of, eSight by Gentex Corporation, which intends to
+ * LICENSE Copyright (C) 2009-2024 by Gentex Technology Canada. All Rights
+ * Reserved.The software and information contained herein are proprietary to, and
+ * comprise valuable trade secrets of, Gentex Technology Canada, which intends to
  * preserve as trade secrets such software and information.
  */
 
 package com.esightcorp.mobile.app.wificonnection
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.esightcorp.mobile.app.ui.R
-import com.esightcorp.mobile.app.ui.components.text.Header1Text
-import com.esightcorp.mobile.app.ui.components.buttons.TextRectangularButton
+import com.esightcorp.mobile.app.ui.components.ItemSpacer
+import com.esightcorp.mobile.app.ui.components.buttons.TextAndIconRectangularButton
 import com.esightcorp.mobile.app.ui.components.containers.BaseScreen
+import com.esightcorp.mobile.app.ui.components.text.Header1Text
+import com.esightcorp.mobile.app.wificonnection.state.WifiType
+import com.esightcorp.mobile.app.wificonnection.state.WifiTypeUiState
 import com.esightcorp.mobile.app.wificonnection.viewmodels.WifiTypeViewModel
 
 @Composable
 fun WifiTypeRoute(
     navController: NavController, vm: WifiTypeViewModel = hiltViewModel()
 ) {
+    val wifiUiState by vm.uiState.collectAsState()
+
     WifiTypeScreen(
         navController = navController,
         modifier = Modifier,
+        wifiUiState = wifiUiState,
         onTypePressed = vm::onTypePressed,
     )
 }
@@ -37,10 +48,9 @@ fun WifiTypeRoute(
 internal fun WifiTypeScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
-    onTypePressed: (NavController, String) -> Unit,
+    wifiUiState: WifiTypeUiState,
+    onTypePressed: (NavController, Int) -> Unit,
 ) {
-
-
     BaseScreen(
         modifier = modifier,
         showBackButton = false ,
@@ -48,49 +58,63 @@ internal fun WifiTypeScreen(
         onBackButtonInvoked = { /*unused*/ },
         onSettingsButtonInvoked = { /*unused*/ },
         isBottomButtonNeeded = false,
-        bottomButton = { Unit }) {
+        bottomButton = { }) {
         WifiTypeBody(
             modifier = modifier,
             navController = navController,
+            wifiUiState = wifiUiState,
             onTypePressed = onTypePressed,
         )
 
     }
+}
 
 
+@Preview
+@Composable
+private fun WifiTypePreview() {
+    WifiTypeBody(modifier = Modifier,
+        navController = rememberNavController(),
+        wifiUiState = WifiTypeUiState(),
+        onTypePressed = {_, _ ->  }
+    )
 }
 
 @Composable
 private fun WifiTypeBody(
     modifier: Modifier = Modifier,
     navController: NavController,
-    onTypePressed: (NavController, String) -> Unit,
+    wifiUiState: WifiTypeUiState,
+    onTypePressed: (NavController, Int) -> Unit,
 ){
-    ConstraintLayout(
-        modifier = modifier
-    ) {
-        val (header, wpa, wep, none) = createRefs()
-        Header1Text(text = stringResource(id = R.string.kWifiViewControllerSelectSecurityLabelText),
-            modifier = Modifier.constrainAs(header) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            })
-        TextRectangularButton(onClick = { onTypePressed(navController, "WPA/WPA2") }, modifier = modifier.constrainAs(wpa) {
-            top.linkTo(header.bottom, margin = 35.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }, text = stringResource(id = R.string.kWifiSecurityTypeWPA))
-        TextRectangularButton(onClick = { onTypePressed(navController, "WEP") }, modifier = modifier.constrainAs(wep) {
-            top.linkTo(wpa.bottom, margin = 20.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }, text = stringResource(id = R.string.kWifiSecurityTypeWEP))
-        TextRectangularButton(onClick = { onTypePressed(navController, "None") }, modifier = modifier.constrainAs(none) {
-            top.linkTo(wep.bottom, margin = 20.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }, text = stringResource(id = R.string.kWifiSecurityTypeNone))
+    Column {
+        Header1Text(
+            stringResource(R.string.kWifiViewControllerSelectSecurityLabelText),
+            modifier,
+        )
 
+        ItemSpacer(25.dp)
+        TextAndIconRectangularButton(
+            onClick = { onTypePressed(navController, WifiType.WAP.stringValueResId) },
+            modifier = modifier,
+            icon = if(wifiUiState.wifiType == WifiType.WAP) Icons.Rounded.Check else null,
+            text = stringResource(id = R.string.kWifiSecurityTypeWPA)
+        )
+
+        ItemSpacer(20.dp)
+        TextAndIconRectangularButton(
+            onClick = { onTypePressed(navController, WifiType.WEP.stringValueResId) },
+            modifier = modifier,
+            icon = if(wifiUiState.wifiType == WifiType.WEP) Icons.Rounded.Check else null,
+            text = stringResource(id = R.string.kWifiSecurityTypeWEP)
+        )
+
+        ItemSpacer(20.dp)
+        TextAndIconRectangularButton(
+            onClick = { onTypePressed(navController, WifiType.NONE.stringValueResId) },
+            modifier = modifier,
+            icon = if(wifiUiState.wifiType == WifiType.NONE) Icons.Rounded.Check else null,
+            text = stringResource(id = R.string.kWifiSecurityTypeNone)
+        )
     }
 }
--- modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/repositories/WifiConnectionRepository.kt
+++ modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/repositories/WifiConnectionRepository.kt
@@ -1,8 +1,7 @@
 /*
- * LICENSE
- * Copyright (C) 2009-2024 by eSight by Gentex Corporation. All Rights Reserved.
- * The software and information contained herein are proprietary to, and
- * comprise valuable trade secrets of, eSight by Gentex Corporation, which intends to
+ * LICENSE Copyright (C) 2009-2024 by Gentex Technology Canada. All Rights
+ * Reserved.The software and information contained herein are proprietary to, and
+ * comprise valuable trade secrets of, Gentex Technology Canada, which intends to
  * preserve as trade secrets such software and information.
  */
 
@@ -159,7 +158,7 @@ class WifiConnectionRepository @Inject constructor(
         wifiModel.stopWifiScan()
     }
 
-    fun setWifiType(type: String) {
+    fun setWifiType(type: Int) {
         WifiCache.credentials.setWifiType(type)
     }
 
@@ -197,6 +196,10 @@ class WifiConnectionRepository @Inject constructor(
 
     fun setWifiPassword(pwd: String) {
         wifiModel.setWifiPassword(pwd)
+    }
+
+    fun setWifiNetwork(ssid: String, securityType: Int, password: String) {
+        WifiCache.credentials.setNetwork(ssid, securityType, password)
     }
 
     //endregion

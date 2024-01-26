7cef0c01996644476a5f9ae1c1841a2ced11f78f
raymond zhuang <zhuanglm@gmail.com>
2024-01-26 12:25:50 -0500

    EG-2469: enabled create wifi code in AdvancedWifiScreen; allow to storage a wifi network with ssid/security type; new AdvancedWifiScreen, WifiTypeScreen; fixed ssid and password fields behaviour; changed wifi security type to match resources; new icon button;

--- modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/AdvancedWifiScreen.kt
+++ modules/feature/wificonnection/src/main/java/com/esightcorp/mobile/app/wificonnection/AdvancedWifiScreen.kt
@@ -1,32 +1,35 @@
 /*
- * LICENSE
- * Copyright (C) 2009-2024 by eSight by Gentex Corporation. All Rights Reserved.
- * The software and information contained herein are proprietary to, and
- * comprise valuable trade secrets of, eSight by Gentex Corporation, which intends to
+ * LICENSE Copyright (C) 2009-2024 by Gentex Technology Canada. All Rights
+ * Reserved.The software and information contained herein are proprietary to, and
+ * comprise valuable trade secrets of, Gentex Technology Canada, which intends to
  * preserve as trade secrets such software and information.
  */
 
 package com.esightcorp.mobile.app.wificonnection
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.esightcorp.mobile.app.ui.R
+import com.esightcorp.mobile.app.ui.components.ItemSpacer
 import com.esightcorp.mobile.app.ui.components.buttons.TextRectangularButton
 import com.esightcorp.mobile.app.ui.components.containers.BaseScreen
 import com.esightcorp.mobile.app.ui.components.text.CustomEditText
 import com.esightcorp.mobile.app.ui.components.text.Header1Text
 import com.esightcorp.mobile.app.ui.components.text.PasswordEditText
 import com.esightcorp.mobile.app.wificonnection.state.WifiAdvancedSettingsUiState
+import com.esightcorp.mobile.app.wificonnection.state.WifiType
 import com.esightcorp.mobile.app.wificonnection.viewmodels.AdvancedWifiViewModel
 
 @Composable
@@ -74,8 +77,9 @@ internal fun AdvancedWifiScreen(
         onBackButtonInvoked = { onBackButtonPressed(navController) },
         onSettingsButtonInvoked = { /*Unused*/ },
         isBottomButtonNeeded = false,
-        bottomButton = { Unit }) {
+        bottomButton = { }) {
 
+        ItemSpacer(35.dp)
         AdvancedWifiBody(
             modifier = modifier,
             navController = navController,
@@ -89,6 +93,18 @@ internal fun AdvancedWifiScreen(
     }
 }
 
+@Preview
+@Composable
+private fun WifiTypePreview() {
+    AdvancedWifiBody(modifier = Modifier,
+        navController = rememberNavController(),
+        wifiUiState = WifiAdvancedSettingsUiState(),
+        onSsidUpdated = {_ ->  },
+        onPasswordUpdated = {_ ->},
+        onSecurityButtonPressed = {_ ->},
+        onFinishButtonPressed = { _ ->  }
+    )
+}
 @Composable
 private fun AdvancedWifiBody(
     modifier: Modifier,
@@ -98,70 +114,52 @@ private fun AdvancedWifiBody(
     onPasswordUpdated: (String) -> Unit,
     onSecurityButtonPressed: (NavController) -> Unit,
     onFinishButtonPressed: (NavController) -> Unit,
-
-    ) {
-    ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val (ssidHeader, ssidField, pwdHeader, pwdField, securityHeader, securityButton, finishButton) = createRefs()
-
+){
+    Column {
         Header1Text(
-            text = stringResource(id = R.string.kWifiViewWifiNameLabelText),
-            modifier = modifier.constrainAs(ssidHeader) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-            })
+            stringResource(R.string.kWifiViewWifiNameLabelText),
+            modifier,
+        )
 
+        ItemSpacer(25.dp)
         CustomEditText(value = wifiUiState.ssid,
             onValueChange = onSsidUpdated,
-            modifier = modifier.constrainAs(ssidField) {
-                top.linkTo(ssidHeader.bottom, margin = 10.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }.fillMaxWidth())
-        if(!wifiUiState.wifiType.equals("none", ignoreCase = false)){
+            modifier = modifier.fillMaxWidth())
+
+        if(wifiUiState.wifiType != WifiType.NONE) {
+            ItemSpacer(35.dp)
             Header1Text(
                 text = stringResource(id = R.string.kWifiViewWifiPasswordLabelText),
-                modifier = modifier.constrainAs(pwdHeader) {
-                    top.linkTo(ssidField.bottom, margin = 35.dp)
-                    start.linkTo(parent.start)
-                })
+                modifier = modifier
+            )
+
+            ItemSpacer(25.dp)
             PasswordEditText(
                 value = wifiUiState.password,
                 onValueChange = onPasswordUpdated,
-                modifier = modifier.constrainAs(pwdField) {
-                    top.linkTo(pwdHeader.bottom, margin = 10.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }.fillMaxWidth())
+                modifier = modifier.fillMaxWidth()
+            )
         }
 
+        ItemSpacer(35.dp)
         Header1Text(
             text = stringResource(id = R.string.kWifiViewControllerSelectSecurityLabelText),
-            modifier = modifier.constrainAs(securityHeader) {
-                top.linkTo(pwdField.bottom, margin = 35.dp)
-                start.linkTo(parent.start)
-            })
+            modifier = modifier)
 
+        ItemSpacer(25.dp)
         TextRectangularButton(
             onClick = { onSecurityButtonPressed(navController) },
-            modifier = modifier.constrainAs(securityButton) {
-                top.linkTo(securityHeader.bottom, margin = 10.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            },
-            text = wifiUiState.wifiType
+            modifier = modifier.wrapContentWidth(),
+            text = stringResource(id = wifiUiState.wifiType.stringValueResId)
         )
 
+        ItemSpacer(35.dp)
         TextRectangularButton(
             onClick = { onFinishButtonPressed(navController)},
-            modifier = modifier.constrainAs(finishButton) {
-                top.linkTo(securityButton.bottom, margin = 35.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            },
-            enabled = false,
+            modifier = modifier.wrapContentWidth(),
+            enabled = wifiUiState.isPasswordValid,
             text = stringResource(id = R.string.kCreateWifiCodeButtonText)
         )
 
     }
-
 }

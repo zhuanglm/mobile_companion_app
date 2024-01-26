7aea174b8348215d98526480295ba22f079a0d04
raymond zhuang <zhuanglm@gmail.com>
2024-01-26 14:47:10 -0500

    EG-2469: QR flow, EnterPasswordScreen button change to Create wifi code; fixed getQRString wifi type issue;

--- modules/core/networking/src/main/java/com/esightcorp/mobile/app/networking/WifiModel.kt
+++ modules/core/networking/src/main/java/com/esightcorp/mobile/app/networking/WifiModel.kt
@@ -1,5 +1,6 @@
 /*
- * LICENSE Copyright (C) 2009-2024 by Gentex Technology Canada. All Rights
+ * LICENSE
+ * Copyright (C) 2009-2024 by Gentex Technology Canada. All Rights
  * Reserved.The software and information contained herein are proprietary to, and
  * comprise valuable trade secrets of, Gentex Technology Canada, which intends to
  * preserve as trade secrets such software and information.

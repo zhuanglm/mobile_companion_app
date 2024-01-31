310b6cbf6b2e0c0ecaf4aea4ca30829f2a0cf22b
raymond zhuang <zhuanglm@gmail.com>
2024-01-31 18:49:17 -0500

    EG-1474: set logo to be unreadable; set DeviceCard to be unreadable; set device button at BTDevicesScreen specific read resource;

--- modules/core/ui/src/main/java/com/esightcorp/mobile/app/ui/components/DeviceCard.kt
+++ modules/core/ui/src/main/java/com/esightcorp/mobile/app/ui/components/DeviceCard.kt
@@ -22,11 +22,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp

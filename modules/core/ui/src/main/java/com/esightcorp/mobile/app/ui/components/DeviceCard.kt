13f4cefa5b28efc51b46bc912255c9c3c4f442bf
raymond zhuang <zhuanglm@gmail.com>
2024-02-01 17:00:40 -0500

    EG-1474: added a non clickable element to avoid TalkBack narrate "button" when it could not be clicked;

--- modules/core/ui/src/main/java/com/esightcorp/mobile/app/ui/components/DeviceCard.kt
+++ modules/core/ui/src/main/java/com/esightcorp/mobile/app/ui/components/DeviceCard.kt
@@ -22,10 +22,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,6 +78,7 @@ fun DeviceCard(
                         .semantics {
                             //avoid talkBack read it as "double click to active"
                             contentDescription = if (isClickable) readLabel else ""
+                            role = Role.Image
                         }
                         .weight(1F), horizontalAlignment = Alignment.Start
                 ) {

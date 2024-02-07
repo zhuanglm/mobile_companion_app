07518006375f16831da79a470ebecf29391a811c
Phan Dang Duy Thinh <phduxa@syntronic.com>
2024-02-07 11:56:31 -0500

    Bluetooth - Update NoDevicesConnected route to remove bluetooth usage

--- modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/NoDevicesConnected.kt
+++ modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/NoDevicesConnected.kt
@@ -8,12 +8,8 @@
 
 package com.esightcorp.mobile.app.btconnection
 
-import android.util.Log
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,10 +23,6 @@ import com.esightcorp.mobile.app.ui.components.buttons.TermsAndPolicy
 import com.esightcorp.mobile.app.ui.components.buttons.bottomButtons.FeedbackButton
 import com.esightcorp.mobile.app.ui.components.containers.BaseScreen
 import com.esightcorp.mobile.app.ui.components.text.PersonalGreeting
-import com.esightcorp.mobile.app.ui.extensions.BackStackLogger
-import com.esightcorp.mobile.app.ui.extensions.navigate
-import com.esightcorp.mobile.app.ui.navigation.BtConnectionNavigation
-import com.esightcorp.mobile.app.ui.navigation.HomeNavigation
 import com.esightcorp.mobile.app.ui.navigation.OnActionCallback
 import com.esightcorp.mobile.app.ui.navigation.OnNavigationCallback
 
@@ -39,63 +31,21 @@ fun NoDeviceConnectedRoute(
     navController: NavController,
     vm: NoDevicesConnectedViewModel = hiltViewModel(),
 ) {
-    // Get Bluetooth UI state from view model
-    val btUiState by vm.uiState.collectAsState()
-
-    BackStackLogger(navController, TAG)
-
-    Log.d(
-        TAG,
-        "NoDeviceConnectedRoute - isBtEnabled: ${btUiState.isBtEnabled}\nconnectionStatus: ${btUiState.btConnectionStatus}"
+    NoDeviceConnectedScreen(
+        onScanESightPressed = vm::navigateToScanESight,
+        onSettingsButtonPressed = vm::navigateToSettings,
+        onFeedbackButtonPressed = vm::showFeedbackPage,
+        onTermsAndConditionsPressed = {
+            //TODO: implement this!!!
+        },
+        onPrivacyPolicyPressed = {
+            //TODO: implement this!!!
+        },
+        navController = navController
     )
-
-    // Check if Bluetooth is enabled
-    when (btUiState.isBtEnabled) {
-        false -> {
-            // If Bluetooth is not enabled, show the Bluetooth enabled screen
-            Log.d(TAG, "NoDeviceConnectedRoute: Bluetooth not enabled")
-            NavigateBluetoothDisabled(navController = navController)
-        }
-
-        true -> when (btUiState.btConnectionStatus) {
-            false -> {
-                // If Bluetooth is enabled and there is no device connected, show the no device connected screen
-                Log.d(TAG, "NoDeviceConnectedRoute: Bluetooth enabled but not connected ")
-                NoDeviceConnectedScreen(
-                    onScanESightPressed = vm::navigateToScanESight,
-                    onSettingsButtonPressed = vm::navigateToSettings,
-                    onFeedbackButtonPressed = vm::showFeedbackPage,
-                    onTermsAndConditionsPressed = vm::openTerms,
-                    onPrivacyPolicyPressed = vm::openPrivacyPolicy,
-                    navController = navController
-                )
-            }
-
-            true -> {
-                Log.i(TAG, "NoDeviceConnectedRoute: Navigate Home should be called.")
-
-                // If Bluetooth is enabled and there is a device connected, navigate to the home screen
-                LaunchedEffect(Unit) {
-                    navController.navigate(
-                        HomeNavigation.FirstScreenRoute,
-                        popUntil = BtConnectionNavigation.IncomingRoute,
-                    )
-                }
-            }
-        }
-    }
 }
 
-// Use LaunchedEffect to navigate to Bluetooth disabled screen after a delay
-@Composable
-fun NavigateBluetoothDisabled(navController: NavController) = LaunchedEffect(Unit) {
-    navController.navigate(BtConnectionNavigation.BtDisabledScreen)
-}
-
-//region Internal implementation
-
-private const val TAG = "BluetoothScreens"
-
+//region Private implementation
 @Composable
 private fun NoDeviceConnectedScreen(
     modifier: Modifier = Modifier,
--- modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/viewmodels/NoDevicesConnectedViewModel.kt
+++ modules/feature/btconnection/src/main/java/com/esightcorp/mobile/app/btconnection/viewmodels/NoDevicesConnectedViewModel.kt
@@ -8,14 +8,8 @@
 
 package com.esightcorp.mobile.app.btconnection.viewmodels
 
-import android.annotation.SuppressLint
 import android.app.Application
-import android.bluetooth.BluetoothDevice
-import android.util.Log
 import androidx.navigation.NavController
-import com.esightcorp.mobile.app.btconnection.repositories.BluetoothConnectionRepositoryCallback
-import com.esightcorp.mobile.app.btconnection.repositories.BtConnectionRepository
-import com.esightcorp.mobile.app.btconnection.state.BluetoothUiState
 import com.esightcorp.mobile.app.ui.R
 import com.esightcorp.mobile.app.ui.components.openExternalUrl
 import com.esightcorp.mobile.app.ui.components.viewmodel.ESightBaseViewModel
@@ -23,69 +17,12 @@ import com.esightcorp.mobile.app.ui.extensions.navigate
 import com.esightcorp.mobile.app.ui.navigation.BtConnectionNavigation
 import com.esightcorp.mobile.app.ui.navigation.SettingsNavigation
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class NoDevicesConnectedViewModel @Inject constructor(
     private val application: Application,
-    btConnectionRepository: BtConnectionRepository,
 ) : ESightBaseViewModel(application) {
-    private val _tag = this.javaClass.simpleName
-
-    /**
-     * Object which is used by the compose UI to track UI State
-     */
-    private var _uiState = MutableStateFlow(BluetoothUiState())
-    val uiState: StateFlow<BluetoothUiState> = _uiState.asStateFlow()
-
-    /**
-     * Interface to receive callbacks from the bluetooth repository
-     */
-    private val btRepositoryListener = object : BluetoothConnectionRepositoryCallback {
-        @SuppressLint("MissingPermission")
-        override fun onDeviceConnected(device: BluetoothDevice?, connected: Boolean?) {
-            updateBtConnectedState(connected, device?.name)
-        }
-
-        override fun onBtStateUpdate(enabled: Boolean) {
-            Log.d(_tag, "onBtStateUpdate: $enabled")
-            updateBtEnabledState(enabled)
-        }
-    }
-
-    /**
-     * First constructor is init{}
-     */
-    init {
-        Log.d(_tag, ": INIT NoDevicesConnectedViewModel")
-        btConnectionRepository.registerListener(btRepositoryListener)
-        btConnectionRepository.setupBtModelListener()
-    }
-
-    //region Private implementation
-
-    /**
-     * Methods to interact with the UI state StateFlow object
-     */
-
-    private fun updateBtEnabledState(state: Boolean) = _uiState.update { currentState ->
-        currentState.copy(isBtEnabled = state)
-    }
-
-    private fun updateBtConnectedState(state: Boolean?, device: String?) =
-        _uiState.update { currentState ->
-            val connStatus = (state == true)
-            when (device) {
-                null -> currentState.copy(btConnectionStatus = connStatus)
-                else -> currentState.copy(btConnectionStatus = connStatus, connectedDevice = device)
-            }
-        }
-
-    //endregion
 
     //region Navigation
 
@@ -112,4 +49,5 @@ class NoDevicesConnectedViewModel @Inject constructor(
     }
 
     //endregion
+
 }

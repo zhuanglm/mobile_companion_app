7a3f0fa11f3480bd2331d215c4a7c7bdc426c212
Phan Dang Duy Thinh <phduxa@syntronic.com>
2024-02-08 15:30:34 -0500

    Refactor source code

--- modules/feature/eshare/src/main/java/com/esightcorp/mobile/app/eshare/composables/EshareConnectedRoute.kt
+++ modules/feature/eshare/src/main/java/com/esightcorp/mobile/app/eshare/composables/EshareConnectedRoute.kt
@@ -45,7 +45,7 @@ import com.esightcorp.mobile.app.ui.R
 import com.esightcorp.mobile.app.ui.components.eshare.AutoFitTextureView
 import com.esightcorp.mobile.app.ui.components.eshare.remote.ColorContrastButton
 import com.esightcorp.mobile.app.ui.components.eshare.remote.EshareRemote
-import com.esightcorp.mobile.app.ui.components.eshare.rotateScreen
+import com.esightcorp.mobile.app.utils.rotateScreen
 import com.esightcorp.mobile.app.ui.components.loading.LoadingScreenWithSpinner
 import com.esightcorp.mobile.app.ui.extensions.BackStackLogger
 import com.esightcorp.mobile.app.ui.navigation.OnActionCallback
